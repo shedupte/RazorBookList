8fc2965e5cef0d3523b19f06d8d8d4ceee2b4bab
shedupte <shedupte@gmail.com>
2020-07-25 20:45:09 -0400

    Web Api added

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RazorBookList.Model;
 
 namespace RazorBookList.Controllers
 {
+    [Route("api/Book")]
     public class BookController : Controller
     {
-        public IActionResult Index()
+        private readonly ApplicationDbContext _db;
+
+        public BookController(ApplicationDbContext db)
         {
-            return View();
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Book.ToListAsync() });
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Book.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null) 
+            {
+                return Json(new { sucess = false, message = "Error while Deleting" });
+            }
+            _db.Book.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
         }
     }
 }
